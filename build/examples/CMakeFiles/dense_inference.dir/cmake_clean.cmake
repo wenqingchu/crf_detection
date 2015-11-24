d97e04b3a1099320f3c4d8de25b192323cd64cd1
chuwenqing <571869993@qq.com>
2015-11-24 21:19:23 +0000

    first commit

---
+++ build/examples/CMakeFiles/dense_inference.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/dense_inference.dir/util.cpp.o"
+  "CMakeFiles/dense_inference.dir/dense_inference.cpp.o"
+  "dense_inference.pdb"
+  "dense_inference"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/dense_inference.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
